74ff38d4b611a3937d91de86d50c8a2d67c7b257
cẩm nguyễn <cam.IT.epu@outlook.com>
2017-04-27 10:54:05 +0700

    a

--- DTO1/NhacCCDAO.vb
+++ DTO1/NhacCCDAO.vb
@@ -1,6 +1,14 @@
-﻿Public Class NhacCCDAO
+﻿
+Imports DAO1
+
+
+Public Class NhacCCDAO
     Dim con As New config
     Public Function LoadData() As DataTable
         Return con.GetData("dbo.sp_loadNhacCC")
+    End Function
+    Public Function Add(ByVal NCCDTO As NhacCCDAO) As Integer
+
+        Return Nothing
     End Function
 End Class
--- WindowsApplication2/UIlayer/NhaCCform.vb
+++ WindowsApplication2/UIlayer/NhaCCform.vb
@@ -1,15 +1,10 @@
-﻿Imports BUS1
-Imports DAO1
-Imports DTO1
+﻿
 
 
 
 Public Class NhaCCform
-    Dim con As New config
-    Dim NCCBUS As New NhaCCBUS
-    Dim NCC As New NhaCCDTO
+
     Private Sub NhaCCform_Load(sender As Object, e As EventArgs) Handles Me.Load
-        dgvNhaCC.DataSource = NCCBUS.GetData
 
     End Sub
 
