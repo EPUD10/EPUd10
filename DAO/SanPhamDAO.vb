c1d503c6ec6f902ff82e2beac7580d3e260c36dd
Trần Thị Diệu Ninh <ttdn1997@gmail.com>
2017-05-06 23:20:42 +0700

    aaaaaaaaa

--- DAO/SanPhamDAO.vb
+++
@@ -1,8 +0,0 @@
-﻿Public Class SanPhamDAO
-    Private cls As New clsketNoi
-    Public Function LoadData() As DataTable
-        'ok
-        Return cls.getData("sp_LoadSP") 'ok
-    End Function
-End Class
-
